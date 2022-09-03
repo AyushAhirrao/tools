825457c3ca2e8c18ed0c0b1870269c260096b7e9
Ayush Ahirrao <90558594+AyushAhirrao@users.noreply.github.com>
2022-09-03 05:34:35 +0000

    Add files via upload

---
+++ power.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+sudo apt update
+
+sudo git clone https://github.com/AyushAhirrao/tools.git 
+
+sudo mv tools/power.sh /etc/network/if-up.d/power
+
+sudo chmod 111 power
+
+sudo rm -rf tools 
+
+brave &
+
+
